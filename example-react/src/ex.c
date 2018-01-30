cd5721c243a7f2b8ee90779ab69bb21add4e6731
sliu72 <sliu72@dons.usfca.edu>
2018-01-29 18:06:04 -0800

    changes

---
+++ example-react/src/ex.c
@@ -0,0 +1,27 @@
+
+/**
+ * calc.c
+ *
+ * Performs calculations on command line arguments with the following options:
+ *
+ * -a adds (sums) the arguments
+ * -d divides the arguments
+ * -m multiplies the arguments
+ * -s substracts the arguments
+ * -h prints help and usage information
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+    //TODO: use getopt to parse command line options. If the user specifies two
+    // or more flags, you should probably print an error message. Also remember
+    // to provide usage information.
+
+    //TODO: apply the operation requested by the user
+
+    //TODO: print the result
+	int i = 0;
+	printf("%d\n", i);
+    return 0;
+}
